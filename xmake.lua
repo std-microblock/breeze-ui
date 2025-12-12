d5cc64b192cd9a31a4eca7de8475ce309fb192bf
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-12-12 14:02:33 +0800

    refactor: move nanovg and nanosvg to sourcecode

--- xmake.lua
+++ xmake.lua
@@ -7,11 +7,30 @@ add_rules("mode.releasedbg")
 includes("deps/glfw.lua")
 
 add_requires("breeze-glfw", {alias = "glfw"})
-add_requires("nanovg", "glad", "nanosvg")
+add_requires("glad")
+
+target("nanovg")
+    set_kind("static")
+    add_files("src/nanovg/**.c")
+    add_includedirs("src/nanovg", {
+        public = true
+    })
+    add_headerfiles("src/nanovg/*.h")
+
+target("nanosvg")
+    set_kind("headeronly")
+    add_files("src/nanosvg/**.c")
+    add_includedirs("src/nanosvg", {
+        public = true
+    })
+    add_headerfiles("src/nanosvg/*.h")
 
 target("breeze_ui")
     set_kind("static")
-    add_packages("glfw", "glad", "nanovg", "nanosvg", {
+    add_packages("glfw", "glad", {
+        public = true
+    })
+    add_deps("nanovg", "nanosvg", {
         public = true
     })
     add_syslinks("dwmapi", "shcore")
