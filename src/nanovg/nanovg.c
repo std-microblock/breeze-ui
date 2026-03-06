4c734770d1da1d66a60607fa50ccbca7bf779648
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-03-07 03:24:58 +0800

    Revert "upd"
    This reverts commit 3522129596f8381ab283c6d8b479f0cd8ad13fd7.

--- src/nanovg/nanovg.c
+++ src/nanovg/nanovg.c
@@ -667,7 +667,7 @@ void nvgReset(NVGcontext* ctx)
 	state->letterSpacing = 0.0f;
 	state->lineHeight = 1.0f;
 	state->fontBlur = 0.0f;
-	state->textAlign = NVG_ALIGN_LEFT | NVG_ALIGN_BOTTOM;
+	state->textAlign = NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE;
 	state->fontId = 0;
 }
 
